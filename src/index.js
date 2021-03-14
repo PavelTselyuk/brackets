9f9c3eed1773c9cd38ce5e692044bb22219629ce
Pavel Tselyuk <pavlik_astap@mail.ru>
2021-03-14 20:03:55 +0300

    fix: fully done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // if (str.length % 2 != 0) return false;
-  let brackets = {  }
-  for(let i=0; i<bracketsConfig.length; i++){
-    brackets[bracketsConfig[i][0]]=bracketsConfig[i][1];
+  if (str.length % 2 != 0) return false;
+  let brackets = {}
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
-  console.log(brackets);
   let arrFromGivenStr = str.split('');
   let index = 0;
 
-  for (index; index < arrFromGivenStr.length;) {
-    if (arrOfBrackets.indexOf(arrFromGivenStr[index]) > -1) {
-      if (arrFromGivenStr[index + 1] == brackets[arrFromGivenStr[index]]) {
+  for (index; index < arrFromGivenStr.length; index++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arrFromGivenStr[index] == bracketsConfig[j][0] && arrFromGivenStr[index + 1] == bracketsConfig[j][1]) {
         arrFromGivenStr.splice(index, 2);
-        index = 0;
-        continue;
-      }
-    } else {
-      if (arrFromGivenStr[index] == arrFromGivenStr[index + 1]) {
-        arrFromGivenStr.splice(index, 2);
-        index = 0;
+        index = -1;
         continue;
       }
     }
-    index++;
   }
   console.log(arrFromGivenStr);
   return arrFromGivenStr.length > 0 ? false : true;
