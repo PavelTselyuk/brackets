0e3f0e8b585862eb1984dd922f28d3daeefe321f
Pavel Tselyuk <pavlik_astap@mail.ru>
2021-03-14 19:15:25 +0300

    feat: almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // if (str.length % 2 != 0) return false;
+  let brackets = {  }
+  for(let i=0; i<bracketsConfig.length; i++){
+    brackets[bracketsConfig[i][0]]=bracketsConfig[i][1];
+  }
+  console.log(brackets);
+  let arrFromGivenStr = str.split('');
+  let index = 0;
+
+  for (index; index < arrFromGivenStr.length;) {
+    if (arrOfBrackets.indexOf(arrFromGivenStr[index]) > -1) {
+      if (arrFromGivenStr[index + 1] == brackets[arrFromGivenStr[index]]) {
+        arrFromGivenStr.splice(index, 2);
+        index = 0;
+        continue;
+      }
+    } else {
+      if (arrFromGivenStr[index] == arrFromGivenStr[index + 1]) {
+        arrFromGivenStr.splice(index, 2);
+        index = 0;
+        continue;
+      }
+    }
+    index++;
+  }
+  console.log(arrFromGivenStr);
+  return arrFromGivenStr.length > 0 ? false : true;
 }
